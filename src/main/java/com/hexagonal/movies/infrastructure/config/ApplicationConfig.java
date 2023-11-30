176eb8490a4baf46bf748ed28ad53c07ebbf9fc5
XaviRibera <xaviribera2004@gmail.com>
2023-11-30 16:00:56 +0100

    Fixed ApplicationConfig.java

--- src/main/java/com/hexagonal/movies/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/hexagonal/movies/infrastructure/config/ApplicationConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 import com.hexagonal.movies.application.services.MovieService;
 import com.hexagonal.movies.application.usecases.*;
-import com.hexagonal.movies.domain.ports.in.RetrieveMovieUseCase;
 import com.hexagonal.movies.domain.ports.out.MovieRepositoryPort;
 
 @Configuration
@@ -14,10 +13,10 @@ public class ApplicationConfig {
     @Bean
     public MovieService movieService(MovieRepositoryPort movieRepositoryPort){
         return new MovieService(
-            new CreateMovieUseCaseImpl(movieRepositoryPort),
-            new RetriveMovieUseCaseImpl(movieRepositoryPort),
-            new UpdateMovieUseCaseImpl(movieRepositoryPort),
-            new DeleteMovieUseCaseImpl(movieRepositoryPort)
-        );
+            new CreateMovieUseCaseImpl(movieRepositoryPort), 
+            new DeleteMovieUseCaseImpl(movieRepositoryPort),
+            new RetriveMovieUseCaseImpl(movieRepositoryPort), 
+            new UpdateMovieUseCaseImpl(movieRepositoryPort)
+            );
     }
 }
